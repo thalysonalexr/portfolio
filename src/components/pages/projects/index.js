4bb97a714aaeb656690e48ba0472c24803036638
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-01-31 12:24:30 -0400

    removing unnecessary settings

--- src/components/pages/projects/index.js
+++ src/components/pages/projects/index.js
@@ -1,11 +1,8 @@
-import Loading from '@/components/Loading'
 import Error from '@/components/Error'
 
 const Projects = () => ({
   component: import('./Projects'),
-  loading: Loading,
   error: Error,
-  delay: 200,
   timeout: 3000
 })
 
--- src/components/pages/skills/index.js
+++ src/components/pages/skills/index.js
@@ -1,11 +1,8 @@
-import Loading from '@/components/Loading'
 import Error from '@/components/Error'
 
 const Skills = () => ({
   component: import('./Skills'),
-  loading: Loading,
   error: Error,
-  delay: 200,
   timeout: 3000
 })
 
