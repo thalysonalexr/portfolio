7eba7a54eee5dd6b55487baa7e81293eefb07254
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-02-06 19:02:37 -0400

    add vue-wait and fontawesome free icons

--- src/main.js
+++ src/main.js
@@ -1,30 +1,29 @@
 import Vue from 'vue'
+import VueWait from 'vue-wait'
 import App from './App'
 import router from './router'
 import { store } from '@/store/vuex'
 import Style from '@/assets/css/all.css'
+import FontAwesome from '@fortawesome/fontawesome-free/css/all.min.css'
 
 import 'materialize-css'
 import 'material-icons/iconfont/material-icons.css'
 import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize.min.js'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faUserSecret)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
 Vue.use(Style)
+Vue.use(FontAwesome)
+Vue.use(VueWait)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   store,
+  wait: new VueWait({
+    useVuex: true
+  }),
   components: { App },
   template: '<App/>'
 })
